b12281652552adac66de5bdb1d1bea11d4f481ac
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-10-11 17:34:51 +0200

    reading a file 8 bytes at a time

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 def main():
     file = open("./message.txt", "r")
-
-    while True:
-        data = file.read(8)
-        if not data:
-            break;
-        print("read: " + data)
     
-main()
+    string = ""
+    while True:
+        data = bytes(8)
+        n = file.read(len(data))
+        if not n:
+            break;
+        
+        # add reading to data
+        data = data + n.encode()
+        
+        # find \n in data
+        idx = data.find(b'\n')
+        
+        # if we found an index
+        if idx != -1:
+            # save from data to the idx in string
+            string += data[:idx].decode()
+            data = data[idx + 1:]
+            print("read: " + string)
+            string = ""
+
+        string += data.decode()
+
+    if len(string) != 0:
+        print("read: " + string)
+
+if __name__ == "__main__":
+    main()
