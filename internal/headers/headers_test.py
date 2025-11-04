a0c5dbd774a39799f319b58400669c98ea55908f
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-11-04 19:44:34 +0100

    change to .get() + new test for invalid tokens

--- internal/headers/headers_test.py
+++ internal/headers/headers_test.py
@@ -9,8 +9,8 @@ class TestRequest(unittest.TestCase):
         headers = Headers()
         data = bytearray("Host: localhost:1234\r\nTestTest:    msgmsg    \r\n\r\n".encode())
         n, done = headers.parse(data)
-        self.assertEqual("localhost:1234", headers.data["Host"])
-        self.assertEqual("msgmsg", headers.data["TestTest"])
+        self.assertEqual("localhost:1234", headers.get("HOST"))
+        self.assertEqual("msgmsg", headers.get("TestTest"))
         self.assertEqual(49, n)
         self.assertTrue(done)
 
@@ -18,7 +18,15 @@ class TestRequest(unittest.TestCase):
         headers = Headers()
         data = bytearray("      Host : localhost:1234       \r\n\r\n".encode())
         n, done = headers.parse(data)
-        self.assertEqual("localhost:1234", headers.data["Host"])
+        self.assertEqual("localhost:1234", headers.get("HOST"))
+        self.assertEqual(0, n)
+        self.assertFalse(done)
+
+        # Invalid spacing header
+        headers = Headers()
+        data = bytearray("H@st: localhost:1234\r\n\r\n".encode())
+        n, done = headers.parse(data)
+        self.assertEqual("localhost:1234", headers.get("HOST"))
         self.assertEqual(0, n)
         self.assertFalse(done)
 
