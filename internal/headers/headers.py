d8b84efcf870e7275862200f4dbec27f8ef2fd0d
JavierBorrero <javi.borrero.cerro@gmail.com>
2025-11-11 18:20:46 +0100

    add err to headers parse method

--- internal/headers/headers.py
+++ internal/headers/headers.py
@@ -2,6 +2,7 @@ rn = "\r\n".encode()
 NO_ERROR = ""
 ERROR_MALFORMED_FIELD_LINE = "malformed field line"
 ERROR_MALFORMED_FIELD_NAME = "malformed field name"
+ERROR_INVALID_TOKEN = "invalid token in field name"
 
 class Headers:
     headers: dict[str, str]
@@ -26,7 +27,7 @@ class Headers:
             self.headers[name] = value
 
 
-    def parse(self, data: bytearray) -> tuple[int, bool]:
+    def parse(self, data: bytearray) -> tuple[int, bool, str]:
         read = 0
         done = False
 
@@ -44,16 +45,16 @@ class Headers:
 
             name, value, err = parse_header(data[read:read+idx])
             if err != NO_ERROR:
-                return 0, False
+                return 0, False, err
 
             if not is_token(name.encode()):
-                return 0, False
+                return 0, False, ERROR_INVALID_TOKEN
 
             read += idx + len(rn)
             #self.headers[name] = value
             self.set(name, value)
 
-        return read, done
+        return read, done, NO_ERROR
 
 def parse_header(field_line: bytearray) -> tuple[str, str, str]:
 
--- internal/headers/headers_test.py
+++ internal/headers/headers_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from headers import Headers
+from headers import ERROR_INVALID_TOKEN, ERROR_MALFORMED_FIELD_LINE, ERROR_MALFORMED_FIELD_NAME, Headers
 
 class TestRequest(unittest.TestCase):
     def test_header_parse(self):
@@ -8,7 +8,8 @@ class TestRequest(unittest.TestCase):
         # Valid single header
         headers = Headers()
         data = bytearray("Host: localhost:1234\r\nTestTest:    msgmsg    \r\n\r\n".encode())
-        n, done = headers.parse(data)
+        n, done, err = headers.parse(data)
+        self.assertEqual("", err)
         self.assertEqual("localhost:1234", headers.get("HOST"))
         self.assertEqual("msgmsg", headers.get("TestTest"))
         self.assertEqual(49, n)
@@ -17,21 +18,23 @@ class TestRequest(unittest.TestCase):
         # Invalid spacing header
         headers = Headers()
         data = bytearray("      Host : localhost:1234       \r\n\r\n".encode())
-        n, done = headers.parse(data)
+        n, done, err = headers.parse(data)
+        self.assertEqual(err, ERROR_MALFORMED_FIELD_NAME)
         self.assertEqual(0, n)
         self.assertFalse(done)
 
         # Invalid field name token
         headers = Headers()
         data = bytearray("H@st: localhost:1234\r\n\r\n".encode())
-        n, done = headers.parse(data)
+        n, done, err = headers.parse(data)
+        self.assertEqual(err, ERROR_INVALID_TOKEN)
         self.assertEqual(0, n)
         self.assertFalse(done)
 
         # Valid multiple field values
         headers = Headers()
         data = bytearray("Host: localhost:1234\r\nHost: localhost:1234\r\n".encode())
-        n, done = headers.parse(data)
+        n, done, err = headers.parse(data)
         self.assertEqual("localhost:1234,localhost:1234", headers.get("HOST"))
         self.assertFalse(done)
 
