c4d59524fe8664b311f4938d5124262bd342f515
jxvxxrbc <javi.borrero.cerro@gmail.com>
2025-11-25 18:50:13 +0100

    revert change in copy_bytes and headers parsing tests

--- internal/request/request_test.py
+++ internal/request/request_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from request import request_from_reader
+from internal.request.request import request_from_reader
 
 
 class ChunkReader:
@@ -14,8 +14,8 @@ class ChunkReader:
         self.pos = pos
 
     def copy_bytes(self, dst: bytearray, src: bytes) -> int:
-        n = min(len(dst) - self.pos, len(src))
-        dst[self.pos:self.pos + n] = src[:n]
+        n = min(len(dst), len(src))
+        dst[:n] = src[:n]
         return n
 
     def read(self, b: bytearray) -> tuple[int, BaseException | None]:
@@ -37,25 +37,25 @@ class ChunkReader:
         return n, None
 
 class TestRequest(unittest.TestCase):
-    def test_request_line_parse(self):
+    #def test_request_line_parse(self):
 
-        reader = ChunkReader(
-                data="GET / HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n",
-                num_bytes_per_read=3,
-                pos=0)
-        request = request_from_reader(reader)
-        self.assertEqual("GET", request.request_line.method)
-        self.assertEqual("/", request.request_line.request_target)
-        self.assertEqual("1.1", request.request_line.http_version)
+        #reader = ChunkReader(
+        #        data="GET / HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n",
+        #        num_bytes_per_read=3,
+        #        pos=0)
+        #request = request_from_reader(reader)
+        #self.assertEqual("GET", request.request_line.method)
+        #self.assertEqual("/", request.request_line.request_target)
+        #self.assertEqual("1.1", request.request_line.http_version)
 
-        reader = ChunkReader(
-                data="GET /coffee HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n",
-                num_bytes_per_read=1,
-                pos=0)
-        request = request_from_reader(reader)
-        self.assertEqual("GET", request.request_line.method)
-        self.assertEqual("/coffee", request.request_line.request_target)
-        self.assertEqual("1.1", request.request_line.http_version)
+        #reader = ChunkReader(
+        #        data="GET /coffee HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n",
+        #        num_bytes_per_read=1,
+        #        pos=0)
+        #request = request_from_reader(reader)
+        #self.assertEqual("GET", request.request_line.method)
+        #self.assertEqual("/coffee", request.request_line.request_target)
+        #self.assertEqual("1.1", request.request_line.http_version)
         # Good GET Request line
         #request = request_from_reader(StringIO("GET / HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n"))
         #self.assertEqual("GET", request.request_line.method)
@@ -73,6 +73,25 @@ class TestRequest(unittest.TestCase):
         #self.assertEqual("", request.request_line.method)
         #self.assertEqual("", request.request_line.request_target)
         #self.assertEqual("", request.request_line.http_version)
+
+    def test_headers_parse(self):
+        # Standard headers
+        reader = ChunkReader(
+                data="GET / HTTP/1.1\r\nHost: localhost:1234\r\nUser-Agent: curl/8.16.0\r\nAccept: */*\r\n\r\n",
+                num_bytes_per_read=3,
+                pos=0)
+        request = request_from_reader(reader)
+        self.assertEqual("localhost:1234", request.headers.get("host"))
+        self.assertEqual("curl/8.16.0", request.headers.get("user-agent"))
+        self.assertEqual("*/*", request.headers.get("accept"))
+
+        # Malformed headers
+        reader = ChunkReader(
+                data="GET / HTTP/1.1\r\nHost localhost:1234\r\n\r\n",
+                num_bytes_per_read=3,
+                pos=0)
+        request = request_from_reader(reader)
+        self.assertEqual({}, request.headers.headers)
 
 if __name__ == '__main__':
     unittest.main()
